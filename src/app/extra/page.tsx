92c6e742a7a04943594fe53b46ba2db347b49a62
jungdae <136069822+jungdae-s@users.noreply.github.com>
2023-07-14 16:13:26 +0900

    Update page.tsx

--- src/app/extra/page.tsx
+++ src/app/extra/page.tsx
@@ -1,21 +1,5 @@
-'use client'
-// Remember you must use an AuthProvider for 
-// client components to useSession
-import { useSession } from 'next-auth/react'
-import { redirect } from 'next/navigation'
-import UserCard from '../components/UserCard'
+export default async function ExtraPage() {
 
-export default function ExtraPage() {
-    const { data: session } = useSession({
-        required: true,
-        onUnauthenticated() {
-            redirect('/api/auth/signin?callbackUrl=/extra')
-        }
-    })
+    return <h1 className="text-5xl">Extra Page!</h1>
 
-    return (
-        <section className="flex flex-col gap-6">
-            <UserCard user={session?.user} pagetype={"Extra"} />
-        </section>
-    )
 }
